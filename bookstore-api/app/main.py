4d2441d0d850a9cec54a5adcc77f18cbdb814986
Bryan Melo <bmelo650@gmail.com>
2025-02-08 23:09:59 -0800

    Remove todo function

--- bookstore-api/app/main.py
+++ bookstore-api/app/main.py
@@ -12,3 +12,10 @@ async def get_todos(todo_id: int):
     for todo in todos:
         if todo.id == todo_id:
             return { "todo":todo }
+        
+
+@app.delete("/todos/{to_do}")
+async def delete_todo(todo_int: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
